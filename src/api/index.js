75d2ba56b3d4bf5f3300f7bb5c60ae9ac34b8d68
suhyeonglee <sounmind777@gmail.com>
2021-11-20 08:57:05 +0900

    feat(api): add fetching user, forum, question ...

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,31 @@
 export const fetchUser = async ({ email, password }) => {
   const response = await fetch(
-    `localhost:5000/login?email=${email}&password=${password}`,
+    `http://localhost:5000/login?email=${email}&password=${password}`,
   );
 
   return response.json();
+};
+
+export const fetchForum = async ({ page, limit = 5 }) => {
+  const response = await fetch(
+    `http://localhost:5000/forum?_page=${page}&_limit=${limit}`,
+  );
+
+  return response.json();
+};
+
+export const toggleQuestionLike = async ({ id }) => {
+  const response = await fetch(`http://localhost:5000/forum/${id}`, {
+    method: 'PUT',
+  });
+
+  return response.json();
+};
+
+export const deleteQuestion = async ({ id }) => {
+  const reponse = await fetch(`http://localhost:5000/forum/${id}`, {
+    method: 'DELETE',
+  });
+
+  return reponse.json();
 };
